9f60c69a0c48daec0e7161c74944fe91232b46bf
Hannes Haglund <hannes.haglund@autoliv.com>
2017-10-18 11:45:36 +0200

    Fix indentation

--- elisp/mye-keys.el
+++ elisp/mye-keys.el
@@ -33,24 +33,24 @@
   (interactive "p\np")
   (setq arg (if arg arg 1))
   (if (and allow-extend
-	   (or (and (eq last-command this-command) (mark t))
-	       (region-active-p)))
+           (or (and (eq last-command this-command) (mark t))
+               (region-active-p)))
       (set-mark
        (save-excursion
-	 (when (< (mark) (point))
-	   (setq arg (- arg)))
-	 (goto-char (mark))
-	 (forward-word arg)
-	 (point)))
+         (when (< (mark) (point))
+           (setq arg (- arg)))
+         (goto-char (mark))
+         (forward-word arg)
+         (point)))
     (let ((wbounds (bounds-of-thing-at-point 'word)))
       (unless (consp wbounds)
-	(error "No word at point"))
+        (error "No word at point"))
       (if (>= arg 0)
-	  (goto-char (car wbounds))
-	(goto-char (cdr wbounds)))
+          (goto-char (car wbounds))
+        (goto-char (cdr wbounds)))
       (push-mark (save-excursion
-		   (forward-word arg)
-		   (point)))
+                   (forward-word arg)
+                   (point)))
       (activate-mark))))
 
 
