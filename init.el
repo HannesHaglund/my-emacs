d13305f7fa44faa1b135704865cfae1fda9c3119
Hannes Haglund <hannes.haglund@gmail.com>
2017-12-23 14:37:14 +0100

    Look ma, no mice!

---
+++ init.el
@@ -0,0 +1,11 @@
+;; Load stuff
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+(require 'mye-visuals)
+(require 'mye-keys)
+(require 'mye-modes)
+(require 'mye-external-packages)
+(require 'mye-no-mouse)
+(package-initialize)
+
+;; Misc
+(defalias 'yes-or-no-p 'y-or-n-p)
