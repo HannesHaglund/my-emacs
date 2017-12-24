ad20dd8229162cb8584c6a2a2663e32cadd13e3c
Hannes Haglund <hannes.haglund@gmail.com>
2017-12-24 20:21:45 +0100

    Remove mark-current-word binding

--- elisp/mye-keys.el
+++ elisp/mye-keys.el
@@ -25,33 +25,6 @@
   (indent-according-to-mode)
   (next-line))
 
-;; src https://www.emacswiki.org/emacs/MarkCommands
-(defun mark-current-word (&optional arg allow-extend)
-  "Put point at beginning of current word, set mark at end."
-  (interactive "p\np")
-  (setq arg (if arg arg 1))
-  (if (and allow-extend
-           (or (and (eq last-command this-command) (mark t))
-               (region-active-p)))
-      (set-mark
-       (save-excursion
-         (when (< (mark) (point))
-           (setq arg (- arg)))
-         (goto-char (mark))
-         (forward-word arg)
-         (point)))
-    (let ((wbounds (bounds-of-thing-at-point 'word)))
-      (unless (consp wbounds)
-        (error "No word at point"))
-      (if (>= arg 0)
-          (goto-char (car wbounds))
-        (goto-char (cdr wbounds)))
-      (push-mark (save-excursion
-                   (forward-word arg)
-                   (point)))
-      (activate-mark))))
-
-
 ;; src: http://stackoverflow.com/questions/683425/globally-override-key-binding-in-emacs
 (defvar my-keys-minor-mode-map
   (let ((map (make-sparse-keymap)))
@@ -63,7 +36,6 @@
     (define-key map (kbd "C-.") 'wind-bck)
     (define-key map (kbd "C-;") 'other-frame)
     (define-key map (kbd "C-/") 'advertised-undo)
-    (define-key map (kbd "C-:") 'mark-current-word)
     map)
   "my-keys-minor-mode keymap.")
 
