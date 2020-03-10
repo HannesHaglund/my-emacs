bb98612882c6c164c038c73de932d1e32f768d5c
HannesHaglund <hannes.haglund@gmail.com>
2020-03-10 11:15:13 +0100

    Add multiple-cursors hydra

--- init.el
+++ init.el
@@ -40,6 +40,11 @@ Return a list of installed packages or nil for every skipped package."
 (setup-melpa)
 
 ;; ----------------------------------------------------------------
+;; hydra
+;; ----------------------------------------------------------------
+(ensure-package-installed 'hydra)
+
+;; ----------------------------------------------------------------
 ;; helm
 ;; ----------------------------------------------------------------
 (ensure-package-installed 'helm)
@@ -93,11 +98,38 @@ Return a list of installed packages or nil for every skipped package."
 ;; multiple-cursors
 ;; ----------------------------------------------------------------
 (ensure-package-installed 'multiple-cursors)
+(require 'multiple-cursors)
 
 (defun mc/clear-cmds-to-run ()
   (interactive)
   (setq mc/cmds-to-run-once nil)
   (setq mc/cmds-to-run-for-all nil))
+
+(defhydra hydra-multiple-cursors (:hint nil)
+  "
+ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cursor%s(if (> (mc/num-cursors) 1) \"s\" \"\")
+------------------------------------------------------------------
+ [_p_]   Next     [_n_]   Next     [_l_] Edit lines  [_0_] Insert numbers
+ [_P_]   Skip     [_N_]   Skip     [_a_] Mark all    [_A_] Insert letters
+ [_M-p_] Unmark   [_M-n_] Unmark   [_s_] Search      [_c_] Clear commands
+ [Click] Cursor at point       [_q_] Quit"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("s" mc/mark-all-in-region-regexp :exit t)
+  ("c" mc/clear-cmds-to-run)
+  ("0" mc/insert-numbers :exit t)
+  ("A" mc/insert-letters :exit t)
+  ("<mouse-1>" mc/add-cursor-on-click)
+  ;; Help with click recognition in this hydra
+  ("<down-mouse-1>" ignore)
+  ("<drag-mouse-1>" ignore)
+  ("q" nil))
 
 ;; ----------------------------------------------------------------
 ;; restart-emacs
@@ -270,10 +302,7 @@ Return a list of installed packages or nil for every skipped package."
     (define-key map (kbd "C-c p s s")   'helm-do-ag-project-root)
 
     ;; Multiple cursors
-    (define-key map (kbd "C-S-c C-S-c") 'mc/edit-lines)
-    (define-key map (kbd "C->")         'mc/mark-next-like-this)
-    (define-key map (kbd "C-<")         'mc/mark-previous-like-this)
-    (define-key map (kbd "C-S-c C-<")   'mc/mark-all-like-this)
+    (define-key map (kbd "C-c m")       'hydra-multiple-cursors/body)
 
     map)
   "my-keys-minor-mode keymap.")
