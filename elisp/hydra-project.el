f6fef6a7f88c48038b6f273e7588f1af8bbe4f29
Hannes Haglund <hannes.haglund@gmail.com>
2022-06-11 20:46:26 +0200

    Less keypresses when switching projects

--- elisp/hydra-project.el
+++ elisp/hydra-project.el
@@ -1,5 +1,13 @@
 (require 'pretty-hydra)
 
+(defun project-find-project-then-find (dir)
+  "\"Switch\" to another project by running an Emacs command.
+When called in a program, it will use the project corresponding to directory DIR."
+  (interactive (list (project-prompt-project-dir)))
+  (let ((default-directory dir)
+        (project-current-inhibit-prompt t))
+    (call-interactively 'project-find-file)))
+
 (pretty-hydra-define hydra-project (:color teal :quit-key "q"
                                            :title "📁 Project %(project-current)")
   ("Find file"
@@ -17,7 +25,7 @@
    (("x"   project-remove-known-project "remove known project"))
 
    "Project"
-   (("p"   project-switch-project "switch project"))))
+   (("p"   project-find-project-then-find "switch project"))))
 
 (provide 'hydra-project)
 ;;; hydra-project.el ends here
