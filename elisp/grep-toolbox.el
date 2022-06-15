52f5f795b61123da10eca73fe32a639f9e9508df
Hannes Haglund <hannes.haglund@gmail.com>
2022-06-15 23:35:11 +0200

    Fix grep-toolbox

--- elisp/grep-toolbox.el
+++ elisp/grep-toolbox.el
@@ -2,7 +2,6 @@
 (require 'consult)
 
 (defvar grep-toolbox--consult-ripgrep-builder-cmd nil)
-(defvar grep-toolbox--replacement-str "GREP_TOOLBOX_REPLACEMENT_STR")
 
 (defun grep-toolbox--consult-ripgrep-builder (input)
   "Build command line given INPUT."
@@ -15,30 +14,29 @@
                                             ;; Case insensitive if there are no uppercase letters
                                             (not (string-match-p "[[:upper:]]" input)))
                                         (member "--ignore-case" cmd))))
-               (output-cmd (string-replace
-                            grep-toolbox--replacement-str
-                            (string-join (append (and (eq type 'pcre) '("-P")) (list  "-e" (consult--join-regexps re type)) opts) " ")
-                            grep-toolbox--consult-ripgrep-builder-cmd)))
+               (output-cmd (append
+                            cmd
+                            (and (eq type 'pcre) '("-P"))
+                            (list  "-e" (consult--join-regexps re type)) opts)))
     (when re
-      (message output-cmd)
-      (list :command (split-string-and-unquote output-cmd)
+      (list :command output-cmd
             :highlight hl))))
 
-(defun grep-toolbox-consult-ripgrep-cmd (cmd)
+(defun grep-toolbox-consult-ripgrep-cmd (cmd initial)
+  "Run consult-ripgrep but with a specific CMD (and INITIAL input)"
   (let ((grep-toolbox--consult-ripgrep-builder-cmd cmd))
-    (consult--grep "Ripgrep" #'grep-toolbox--consult-ripgrep-builder "" "")))
+    (consult--grep "Ripgrep" #'grep-toolbox--consult-ripgrep-builder "" initial)))
 
-(defun grep-toolbox-make-cmd (dirs extra-flags)
-  (string-join
-   (mapcar #'(lambda (dir) (concat "rg --null --line-buffered --color=never --max-columns=1000 --path-separator /   --smart-case --no-heading --line-number " extra-flags " " dir " " grep-toolbox--replacement-str))
-           dirs)
-   " && "))
-
-(defun grep-toolbox-test ()
-  (interactive)
+(defun consult-grep-multidir (dirs flags initial)
+  "Like consult-grep, but support multiple directories DIRS, a list.  Also FLAGS and INITIAL input."
   (grep-toolbox-consult-ripgrep-cmd
-   (grep-toolbox-make-cmd '("d:/Documents/code/my-emacs/elisp/" "d:/Documents/code/cv") "")))
-
+   (concat "rg --null --line-buffered --color=never --max-columns=1000 --path-separator / "
+           "--smart-case --no-heading --line-number "
+           flags " "
+           ;; The order of dirs has to be reversed for some reason.
+           ;; The last dir in the ripgrep command gets its output first.
+           (string-join (reverse dirs) " ") " ")
+   initial))
 
 (provide 'grep-toolbox)
 ;;; grep-toolbox.el ends here
