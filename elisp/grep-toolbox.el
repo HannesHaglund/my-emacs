63c3254f113b3436909af9149dce643ad2058a7f
Hannes Haglund <hannes.haglund@gmail.com>
2022-06-15 13:16:06 +0200

    Trying to fix grep-toolbox. Not working very well.

--- elisp/grep-toolbox.el
+++ elisp/grep-toolbox.el
@@ -2,6 +2,7 @@
 (require 'consult)
 
 (defvar grep-toolbox--consult-ripgrep-builder-cmd nil)
+(defvar grep-toolbox--replacement-str "GREP_TOOLBOX_REPLACEMENT_STR")
 
 (defun grep-toolbox--consult-ripgrep-builder (input)
   "Build command line given INPUT."
@@ -13,15 +14,15 @@
                                           (let ((case-fold-search nil))
                                             ;; Case insensitive if there are no uppercase letters
                                             (not (string-match-p "[[:upper:]]" input)))
-                                        (member "--ignore-case" cmd)))))
+                                        (member "--ignore-case" cmd))))
+               (output-cmd (string-replace
+                            grep-toolbox--replacement-str
+                            (string-join (append (and (eq type 'pcre) '("-P")) (list  "-e" (consult--join-regexps re type)) opts) " ")
+                            grep-toolbox--consult-ripgrep-builder-cmd)))
     (when re
-      (list :command
-            (append cmd
-                    (and (eq type 'pcre) '("-P"))
-                    (list  "-e" (consult--join-regexps re type))
-                    opts)
+      (message output-cmd)
+      (list :command (split-string-and-unquote output-cmd)
             :highlight hl))))
-
 
 (defun grep-toolbox-consult-ripgrep-cmd (cmd)
   (let ((grep-toolbox--consult-ripgrep-builder-cmd cmd))
@@ -29,14 +30,14 @@
 
 (defun grep-toolbox-make-cmd (dirs extra-flags)
   (string-join
-   (mapcar #'(lambda (dir) (concat "rg --null --line-buffered --color=never --max-columns=1000 --path-separator /   --smart-case --no-heading --line-number " extra-flags " " dir))
+   (mapcar #'(lambda (dir) (concat "rg --null --line-buffered --color=never --max-columns=1000 --path-separator /   --smart-case --no-heading --line-number " extra-flags " " dir " " grep-toolbox--replacement-str))
            dirs)
    " && "))
 
 (defun grep-toolbox-test ()
   (interactive)
   (grep-toolbox-consult-ripgrep-cmd
-   (grep-toolbox-make-cmd '("d:/Documents/code/my-emacs/elisp/" "d:/Documents/code/cv") "-g*aux")))
+   (grep-toolbox-make-cmd '("d:/Documents/code/my-emacs/elisp/" "d:/Documents/code/cv") "")))
 
 
 (provide 'grep-toolbox)
