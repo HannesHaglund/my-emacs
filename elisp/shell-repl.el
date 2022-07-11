5153fdb9ae920c61dd72ea5497699277a00009ef
Hannes Haglund <hannes.haglund@gmail.com>
2022-07-11 16:39:52 +0200

    Output buffer for shell-repl

--- elisp/shell-repl.el
+++ elisp/shell-repl.el
@@ -1,5 +1,11 @@
 (require 'subr-x)
+(require 'cl-lib)
 (require 'dired)
+
+(defvar shell-repl-output-buffer
+  "*shell-repl-log*"
+  "Name of buffer to display shell-repl output in.")
+
 
 (defun shell-repl-read-input (output-to-display)
   "Prompt user for shell command and return it.  Message OUTPUT-TO-DISPLAY."
@@ -37,12 +43,40 @@
 
 
 (defun shell-repl-command-to-string-and-cwd (input)
-  "Run shell command INPUT and return a list containing output and CWD after finishing."
+  "Run shell command INPUT and return a list containing output and CWD."
   (let* ((actual-input (format "%s ; pwd -W" input))
          (output (string-trim (shell-command-to-string actual-input)))
          (output-meat (shell-repl-string-all-except-last-line output))
          (output-pwd (shell-repl-string-last-line output)))
     (list output-meat output-pwd)))
+
+
+(defun shell-repl-append-to-output-buffer (cwd input output)
+  "Append shell-repl output (namely CWD INPUT OUTPUT) to SHELL-REPL-OUTPUT-BUFFER."
+  ;; Create the buffer if it doesn't exist.
+  (unless (get-buffer shell-repl-output-buffer)
+    (with-current-buffer (get-buffer-create shell-repl-output-buffer)
+      (special-mode)
+      (read-only-mode -1)))
+  ;; Append to the buffer.
+  (with-current-buffer (get-buffer shell-repl-output-buffer)
+    (save-excursion
+      (goto-char (point-max))
+      (insert (propertize (format ">>> %s %s: %s\n"
+                                  (format-time-string "%H:%M:%S")
+                                  cwd
+                                  input)
+                          'face 'font-lock-function-name-face))
+      (insert (format "%s\n" output))))
+  ;; Scroll to EOB if it's open in a window.
+  (let ((win (get-buffer-window shell-repl-output-buffer)))
+    (when win
+      (with-selected-window win
+        (goto-char (point-max))
+        ;; Recenter so that text fills the entire window.
+        ;; For some reason, text moves up when the minibuffer expands, kinda ruining the effect.
+        ;; Not sure how to get around that.
+        (recenter -1)))))
 
 
 (defun shell-repl ()
@@ -53,9 +87,9 @@
          (default-directory default-directory)) ; So we can change it later
     (while (not (string-blank-p (setq input (shell-repl-read-input last-output))))
       (let* ((output (shell-repl-command-to-string-and-cwd input))
-             (output-meat (first output))
-             (output-cwd (second output)))
-        (message output-cwd)
+             (output-meat (cl-first output))
+             (output-cwd  (cl-second output)))
+        (shell-repl-append-to-output-buffer default-directory input output-meat)
         (setq default-directory output-cwd)
         (setq last-output output-meat)))
     (when last-output
